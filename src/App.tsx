f1d04f92c087a69de200e469949e96c4e7d03f71
Eduardo <mdo.eduardo@outlook.com>
2021-09-21 01:40:26 -0300

    Implementação da estrutura de Rotas
    Adicionada a dependência react-router-dom e feito a configuração da
    estrutura de rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { PageContainer } from "./components/PageContainer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <PageContainer>
-      <div>Hello World!</div>
-    </PageContainer>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from "../../components/PageContainer";
+
+export function Home() {
+  return (
+    <PageContainer>
+      <div>Hello World!</div>
+    </PageContainer>
+  );
+}
